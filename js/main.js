81297b8b501f39a35539837b65088195de3982fc
bcgs <bruno.bcgs@gmail.com>
2017-07-28 15:39:40 -0300

    Page transition

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 $(document).on('click',function(){
 $('.collapse').collapse('hide');
 })
+
+// Page transition
+$("ul.navbar-nav > li > a").on('click', function(event) {
+    event.preventDefault();
+    $("ul.navbar-nav li").removeClass('active');
+    $(this).parent().addClass('active');
+    $("html, body").animate({scrollTop:$(this.hash).offset().top}, 800);
+});
